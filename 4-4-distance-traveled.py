8cd10d5f2b120834d7ba837ffc10433062df48b7
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-20 15:03:04 -0700

    Updated the comments from lines 2-8

---
+++ 4-4-distance-traveled.py
@@ -0,0 +1,8 @@
+# Program 4-4 Distance Traveled
+# Shaun Hayworth
+# CIS 2
+# 10-20-2019
+# Original source code and revision history can be found at https://github.com/SCHayworth/4-4-Distance-Traveled
+
+# Gets the speed of a vehicle and the total time of travel from the user and displays the total distance
+# traveled at each hour.
