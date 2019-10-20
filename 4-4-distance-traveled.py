fd0b060e1c5b8e7f34535544992cf57be9de5224
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-20 15:37:43 -0700

    First draft of complete program code

--- 4-4-distance-traveled.py
+++ 4-4-distance-traveled.py
@@ -6,3 +6,23 @@
 
 # Gets the speed of a vehicle and the total time of travel from the user and displays the total distance
 # traveled at each hour.
+
+# Create a multiline header for output formatting.
+header = '''
+Hour            Distance Traveled
+---------------------------------
+'''
+
+# Prompt the user for the vehicle's speed in mph
+speed = int(input('What is the speed of the vehicle in mph? '))
+
+# Prompt user for the total time of travel
+time = int(input('How many hours has it traveled? '))
+
+# Print the header
+print(header)
+
+# Calculate the total distance traveled at each hour of travel and display the results in two columns.
+for hour in range(time):
+    distance = hour * speed
+    print(f'{hour:<10f} {distance:>10f}')
